4949d82ae2397593d74b5cdd23fbf72299065b58
Eric Victor H.S <66773585+ericsianipar@users.noreply.github.com>
2020-11-04 19:51:00 +0700

    added navbar+jquery transition

---
+++ js/script.js
@@ -0,0 +1,10 @@
+$('.page-scroll').on('click', function(e) {
+  var tujuan = $(this).attr('href');
+  var elemenTujuan = $(tujuan);
+
+  $('html, body').animate({
+    scrollTop: elemenTujuan.offset().top - 55
+  }, 1000);
+
+  e.preventDefault();
+});
